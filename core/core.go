6bb8de38e2f9fed56192ec64e42fc3a89607a076
kenjitheman <kenjitheman@proton.me>
2023-08-22 20:15:37 +0300

    feat: implement converter core logic

--- core/core.go
+++ core/core.go
@@ -5,6 +5,7 @@ import (
 	"encoding/json"
 	"fmt"
 	"os"
+	"sync"
 )
 
 func Convert(jsonFilePath, outputCsvFileName string) {
@@ -52,5 +53,62 @@ func Convert(jsonFilePath, outputCsvFileName string) {
 }
 
 func AsyncConvert(jsonFilePath, outputCsvFileName string) {
-	fmt.Println(jsonFilePath, outputCsvFileName) // mock | TODO
+	jsonData, err := os.ReadFile(jsonFilePath)
+	if err != nil {
+		fmt.Println("[ERROR] error reading JSON file:", err)
+		return
+	}
+
+	var data []map[string]interface{}
+
+	err = json.Unmarshal(jsonData, &data)
+	if err != nil {
+		fmt.Println("[ERROR] error unmarshaling JSON:", err)
+		return
+	}
+
+	jsonChannel := make(chan map[string]interface{})
+
+	var wg sync.WaitGroup
+
+	const numWorkers = 4 // concurrent Goroutines
+	for i := 0; i < numWorkers; i++ {
+		wg.Add(1)
+		go processJSON(jsonChannel, &wg, outputCsvFileName)
+	}
+
+	for _, item := range data {
+		jsonChannel <- item
+	}
+	close(jsonChannel)
+
+	wg.Wait()
+
+	fmt.Println("[SUCCESS] asynchronous conversion successful !")
+}
+
+func processJSON(
+	jsonChannel <-chan map[string]interface{},
+	wg *sync.WaitGroup,
+	outputCsvFileName string,
+) {
+	defer wg.Done()
+
+	csvFile, err := os.Create(outputCsvFileName)
+	if err != nil {
+		fmt.Println("[ERROR] error creating CSV file:", err)
+		return
+	}
+	defer csvFile.Close()
+
+	csvWriter := csv.NewWriter(csvFile)
+	defer csvWriter.Flush()
+
+	for item := range jsonChannel {
+		row := make([]string, 0)
+		for _, value := range item {
+			row = append(row, fmt.Sprintf("%v", value))
+		}
+		csvWriter.Write(row)
+	}
 }
